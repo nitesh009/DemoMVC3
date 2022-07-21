cb99d86926358e57f5dc55af96e4c2054257a845
nitesh009 <niteshgnit3@gmail.com>
2022-07-21 16:11:58 +0530

    Spring MVC

---
+++ src/main/java/com/test/DemoMVCConfig.java
@@ -0,0 +1,24 @@
+package com.test;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@ComponentScan({"com.test"})
+public class DemoMVCConfig  {
+	
+	@Bean
+	public InternalResourceViewResolver getView() {
+		
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		
+		resolver.setPrefix("/WEB-INF/");
+		resolver.setSuffix(".jsp");
+		
+		
+		return resolver;
+	}
+
+}
---
+++ src/main/java/com/test/DemoWebIntializer.java
@@ -0,0 +1,27 @@
+package com.test;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class DemoWebIntializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		// TODO Auto-generated method stub
+		return new Class[] {DemoMVCConfig.class};
+	}
+	
+	
+	@Override
+	protected String[] getServletMappings() {
+		// TODO Auto-generated method stub
+		return new String[] {"/"};
+	}
+
+
+}
